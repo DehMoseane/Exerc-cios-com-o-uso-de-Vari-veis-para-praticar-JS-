b458bbf924d5a9dc99ce49e3444d186ed7bf9b53
Débora Moseane <141942635+DehMoseane@users.noreply.github.com>
2023-10-03 01:17:40 -0300

    Update 1.calcular_IMC.js

--- Execicios_Variaveis/1.calcular_IMC.js
+++ Execicios_Variaveis/1.calcular_IMC.js
@@ -2,10 +2,16 @@
 
 // IMC =
 
-let peso = 46;
-let altura = 1.49
 
-// altura ** 2
-let imc = peso / Math.pow(altura, 2);
+// Nesta linha, você está definindo uma variável de chamada pesoe atribuindo a ela o valor 46. Este valor representa o peso da pessoa em quilogramas.
+let peso = 46; 
 
+// Aqui, você está definindo uma chamada variável alturae atribuindo a ela o valor 1.49. Este valor representa a altura da pessoa em metros.
+let altura = 1.49 
+
+// altura ** 2 // // Nesta linha, você está calculando o IMC usando a fórmula do IMC, que é peso dividido pela altura ao quadrado.
+//cálculo da altura elevada ao quadrado. Então, você divide o peso pelo resultado disso para obter o IMC
+let imc = peso / Math.pow(altura, 2); 
+                                     
+// Por fim, você imprime o resultado do IMC na tela usando console.log(). A mensagem exibida será algo como "Seu IMC é X", onde X é o valor calculado do IMC.
 console.log(`Seu IMC é ${imc}`);
